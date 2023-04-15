f575046c12f64936c9dbabd4a1d0396e0af2de27
Elisabeth Leonhardt <elisabeth.ingcomp@gmail.com>
2023-04-15 12:20:15 -0300

    adding example for filters and custom hook

---
+++ pages/customHook.js
@@ -0,0 +1,85 @@
+import { useFilteredRicksAndMorties } from "@/lib/customHooks";
+import React, { useState } from "react";
+
+function FiltersWithCustomHook() {
+  const [filter, setFilter] = useState({
+    name: "",
+    status: "",
+    gender: "",
+    species: "",
+    type: "",
+  });
+
+  const { data } = useFilteredRicksAndMorties(filter);
+
+  function onFilterChange(e) {
+    const newObject = {};
+    newObject[e.target.name] = e.target.value;
+    setFilter({ ...filter, ...newObject });
+  }
+
+  return (
+    <div>
+      <h1>Filters with Custom Hook in TanStack-Query</h1>
+      <div className="flex gap-4 flex-wrap">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          className="text-black"
+          name="name"
+          onChange={onFilterChange}
+          value={filter.name}
+        />
+        <label htmlFor="status">Status</label>
+        <select
+          name="status"
+          id="status"
+          className="text-black"
+          onChange={onFilterChange}
+        >
+          <option value="">Choose Status</option>
+          <option value="alive">Alive</option>
+          <option value="dead">Dead</option>
+          <option value="unknown">Unknown</option>
+        </select>
+        <label htmlFor="gender">Gender</label>
+        <select
+          name="gender"
+          id=""
+          className="text-black"
+          onChange={onFilterChange}
+        >
+          <option value="">Choose gender</option>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+          <option value="genderless">Genderless</option>
+          <option value="unknown">Unknown</option>
+        </select>
+        <label htmlFor="species">Species</label>
+        <input
+          type="species"
+          name="species"
+          className="text-black"
+          onChange={onFilterChange}
+        />
+        <label htmlFor="type">Type</label>
+        <input
+          type="type"
+          name="type"
+          className="text-black"
+          onChange={onFilterChange}
+        />
+      </div>
+      <div>
+        {data?.results?.map((character) => (
+          <div key={character.id}>
+            {character.name}, {character.status}, {character.gender},{" "}
+            {character.type}, {character.species}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FiltersWithCustomHook;
