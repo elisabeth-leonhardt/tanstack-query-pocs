4f92506b29a354c948a62cf001fb1a1fde4bf9b7
Elisabeth Leonhardt <elisabeth.ingcomp@gmail.com>
2023-08-03 08:00:41 +0200

    modifying todo

--- components/Todo.js
+++ components/Todo.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import eli from "../assets/eli.webp";
 import rick from "../assets/rick.webp";
 import morty from "../assets/morty.webp";
 import summer from "../assets/summer.webp";
